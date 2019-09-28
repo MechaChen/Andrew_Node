9ddc3dc22b2954cd421500c34d6402ce296a5f8a
Benson <tumlivein@gmail.com>
2019-09-28 21:16:18 +0800

    Challenge

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
 
-fs.writeFileSync('note.txt', 'My name is Benson.');
+fs.writeFileSync('notes.txt', 'My name is Benson.');
+
+// 
+// Challenge: Append a message to notes.txt
+// 
+// 1. Use appendFileSync to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
+
+fs.appendFileSync('notes.txt', ' I live in Taichung');
