4c0551a376ab95bbef9237f8d74c4d798f8ba78d
Benson <tumlivein@gmail.com>
2019-09-28 23:57:31 +0800

    11. Importing npm Modules

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-// const add = require('./utils');
-// const sum = add(4, -2);
-// console.log(sum);
-
-// 
-// Challenge: Define and use a function in a new file
-// 
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js load in and call the function printing message to console
-
+const validator = require('validator');
 const getNotes = require('./notes');
 
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(validator.isEmail('benson@gmail.com'));
+console.log(validator.isEmail('gmail.com'));
+
+console.log(validator.isURL('https://mead.io'));
+console.log(validator.isURL('https/mead.io'));
