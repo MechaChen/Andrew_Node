e544689c683f06b9ff6cfabfa4e4935cf1840be3
Benson <tumlivein@gmail.com>
2019-09-28 23:37:38 +0800

    Challenge

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const add = require('./utils');
+// const add = require('./utils');
+// const sum = add(4, -2);
+// console.log(sum);
 
-const sum = add(4, -2);
-console.log(sum);
+// 
+// Challenge: Define and use a function in a new file
+// 
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From app.js load in and call the function printing message to console
+
+const getNotes = require('./notes');
+
+const msg = getNotes();
+
+console.log(msg);
