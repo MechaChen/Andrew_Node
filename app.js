52377ea487261703e5698ef60c1a5f65460dff41
Benson <tumlivein@gmail.com>
2019-09-28 21:23:59 +0800

    10. Importing Your Own Files: Import 1st File

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-const fs = require('fs');
+require('./utils');
 
-fs.writeFileSync('notes.txt', 'My name is Benson.');
+const name = 'Benson';
 
-// 
-// Challenge: Append a message to notes.txt
-// 
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
-
-fs.appendFileSync('notes.txt', ' I live in Taichung');
+console.log(name);
