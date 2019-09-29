3fa052875ce693fa11bbb8bd3f2ebd9f994d44a4
Benson <tumlivein@gmail.com>
2019-09-29 14:28:49 +0800

    15. Getting Input From Users: Process Argument Vector

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const getNotes = require('./notes');
 const msg = getNotes();
 console.log(msg);
 
-// 
-// Challenge
-// 
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-// 
-// Bonus: Use the docs to mess around with other styles. Make text bold and inversed.
-
 const chalk = require('chalk');
 
 console.log(chalk.green('Success!'));
 console.log(chalk.green.bold('Success!'));
-console.log(chalk.green.bold.inverse('Success!'));
+console.log(chalk.red.bold.inverse('Error!'));
+
+// [CMD] node app.js Benson
+console.log(process.argv);
+// ->
+// [
+//   '/usr/local/bin/node', -> Path to Node.js
+//   '/Users/laicailin/Desktop/Andrew/node-course/notes-app/app.js', -> Path to the File
+//   'Benson' -> Argument
+// ]
