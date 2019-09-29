b92b56b679a81b0c2249159c4926bd1c31d178b3
Benson <tumlivein@gmail.com>
2019-09-29 15:09:46 +0800

    Challenge

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ yargs.command({
   description: 'Add a note',
   handler: function() {
     console.log('Adding a new note');
-  }
+  },
 });
 
 // Create remove command
@@ -20,7 +20,31 @@ yargs.command({
   description: 'Remove a note',
   handler: function() {
     console.log('Removing the note');
-  }
+  },
+});
+
+//
+// Challenge: Add two new commands
+// 
+// 1. Setup to support "list" command (print placeholder message for now)
+// 2. Setup to support "read" command (print placeholder message for now)
+// 3. Test your work by running both commands and ensure correct output
+
+// Create list command
+yargs.command({
+  command: 'list',
+  description: 'List your notes',
+  handler: function() {
+    console.log('Listing out all notes');
+  },
+});
+
+yargs.command({
+  command: 'read',
+  description: 'Read a note',
+  handler: function() {
+    console.log('Reading a note');
+  },
 });
 
 console.log(yargs.argv);
