86b899302c20813065a9961cc9ad98af6898588c
Benson <tumlivein@gmail.com>
2019-09-29 20:45:18 +0800

    18. Storing Data with JSON: Write an JSON File with File System

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,9 @@
+const fs = require('fs');
+
 const book = {
     title: 'Ego is the Enemy',
     author: 'Ryan Holiday',
 };
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
-
-const parseData = JSON.parse(bookJSON);
-console.log(parseData.author);
+fs.writeFileSync('1-json.json', bookJSON);
