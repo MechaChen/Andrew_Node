d48bbd0ed9ad25f8c3c210aac574da51a468f847
Benson <tumlivein@gmail.com>
2019-09-29 14:11:29 +0800

    12. Printing in Color

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const validator = require('validator');
 const getNotes = require('./notes');
 
 const msg = getNotes();
-
 console.log(msg);
 
-console.log(validator.isEmail('benson@gmail.com'));
-console.log(validator.isEmail('gmail.com'));
+// 
+// Challenge
+// 
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk into app.js
+// 3. Use it to print the string "Success!" to the console in green
+// 4. Test your work
+// 
+// Bonus: Use the docs to mess around with other styles. Make text bold and inversed.
 
-console.log(validator.isURL('https://mead.io'));
-console.log(validator.isURL('https/mead.io'));
+const chalk = require('chalk');
+
+console.log(chalk.green('Success!'));
+console.log(chalk.green.bold('Success!'));
+console.log(chalk.green.bold.inverse('Success!'));
