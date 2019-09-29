61ba6a52d0597ed6919438860536fa2080430f20
Benson <tumlivein@gmail.com>
2019-09-29 21:08:00 +0800

    Challenge

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday',
-// };
+// 
+// Challenge
+// 
+// 1. Load and parse JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4. Test your work by viewing data in JSON file
 
-// const bookJSON = JSON.stringify(book);
-// fs.writeFileSync('1-json.json', bookJSON);
-
-const dataBuffer = fs.readFileSync('1-json.json'); // return some binary data
+const dataBuffer = fs.readFileSync('1-json.json');
 const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title);
+const user = JSON.parse(dataJSON);
+
+user.name = 'Benson',
+user.age = 25;
+
+const userJSON = JSON.stringify(user);
+fs.writeFileSync('1-json.json', userJSON);
