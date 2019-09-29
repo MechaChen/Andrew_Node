43bd920d5e361e99173af352dcac526a1134f748
Benson <tumlivein@gmail.com>
2019-09-29 15:01:11 +0800

    16. Arguments Parsing with Yargs: Part l: Yargs Command Handler

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const validator = require('validator');
 const yargs = require('yargs');
 const getNotes = require('./notes');
 
-console.log(process.argv);
+// Change the version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  description: 'Add a note',
+  handler: function() {
+    console.log('Adding a new note');
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  description: 'Remove a note',
+  handler: function() {
+    console.log('Removing the note');
+  }
+});
+
 console.log(yargs.argv);
-
-// [CMD] node app.js --help
-// ->
-// [
-//   '/usr/local/bin/node',
-//   '/Users/laicailin/Desktop/Andrew/node-course/notes-app/app.js',
-//   '--help'
-// ]
-// Options:
-//   --help     Show help                                                 [boolean]
-//   --version  Show version number  
-
-
-// [CMD] node app.js --version
-// ->
-// [
-//   '/usr/local/bin/node',
-//   '/Users/laicailin/Desktop/Andrew/node-course/notes-app/app.js',
-//   '--version'
-// ]
-// 1.0.0
