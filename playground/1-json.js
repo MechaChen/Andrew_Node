3ac79f24ee05fcdb3b1e987ef40183207a3242f2
Benson <tumlivein@gmail.com>
2019-09-29 20:51:44 +0800

    18. Storing Data with JSON: Read JSON File

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+// };
 
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync('1-json.json', bookJSON);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+const dataBuffer = fs.readFileSync('1-json.json'); // return some binary data
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
